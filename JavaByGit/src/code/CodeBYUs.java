e6a9e8e73fb164fd3bc71e94c497c44aa66f22c8
srinivasjava27 <srinivasjava04@gmail.com>
2018-02-27 23:09:42 -0600

    Checking it

--- JavaByGit/src/code/CodeBYUs.java
+++ JavaByGit/src/code/CodeBYUs.java
@@ -4,6 +4,7 @@ public class CodeBYUs {
 	public static void main(String[] args) {
 		System.out.println("this is the new project");
 		System.out.println("this is the new project");
+		System.out.println("this is the new project");
 	}
 
 }
