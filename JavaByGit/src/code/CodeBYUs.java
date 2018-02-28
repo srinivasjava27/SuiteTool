afb4182e0725ad4a25906027274e542cd32376c9
srinivasjava27 <srinivasjava04@gmail.com>
2018-02-27 22:26:14 -0600

    First Change

--- JavaByGit/src/code/CodeBYUs.java
+++ JavaByGit/src/code/CodeBYUs.java
@@ -3,6 +3,7 @@ package code;
 public class CodeBYUs {
 	public static void main(String[] args) {
 		System.out.println("this is the new project");
+		System.out.println("this is the new project");
 	}
 
 }
